16b9dfd06fe464091e06e5182f426e96925c72a6
HeHacz <hehahiyourip@gmail.com>
2019-12-20 11:38:50 +0100

    Update lb.sh

--- terraform/eu-central-1/modules/instances/scripts/lb.sh
+++ terraform/eu-central-1/modules/instances/scripts/lb.sh
@@ -2,12 +2,12 @@
 
 
 #echo  /etc/apache2
-echo "ProxyRequest off" > /etc/apache2/conf.d/lb.conf
-echo "ProxyRequest off" >> /etc/apache2/conf.d/lb.conf
-echo "<Proxy balancer://web >" >> /etc/apache2/conf.d/lb.conf
-echo "BalancerMember http://'${$HTTPD01-IP}'" >> /etc/apache2/conf.d/lb.conf
-echo "BalancerMember http://'${$HTTPD02-IP}'" >> /etc/apache2/conf.d/lb.conf
-echo "ProxySet lvmetod=byrequests" >> /etc/apache2/conf.d/lb.conf
-echo "</Proxy>" >> /etc/apache2/conf.d/lb.conf
+# echo "ProxyRequest off" > /etc/apache2/conf.d/lb.conf
+# echo "ProxyRequest off" >> /etc/apache2/conf.d/lb.conf
+# echo "<Proxy balancer://web >" >> /etc/apache2/conf.d/lb.conf
+# echo "BalancerMember http://'${$HTTPD01-IP}'" >> /etc/apache2/conf.d/lb.conf
+# echo "BalancerMember http://'${$HTTPD02-IP}'" >> /etc/apache2/conf.d/lb.conf
+# echo "ProxySet lvmetod=byrequests" >> /etc/apache2/conf.d/lb.conf
+# echo "</Proxy>" >> /etc/apache2/conf.d/lb.conf
 
 systemclt restart apache2
